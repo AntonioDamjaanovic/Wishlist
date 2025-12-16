72fe30f406a9a9a4ffa15b2c2201e8deb622cec0
AntonioDamjaanovic <damjanovic1109@gmail.com>
2025-12-16 13:16:37 +0100

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Antonio Damjanović on 16.12.2025..
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
